f911c00b48259abc1fb8114f6898d80e3756bb73
LaymajouxClement <laymajouxclement@gmail.com>
2020-04-21 11:24:58 +0200

    add role -ROLE_EDITOR  + modification roles

--- site/src/Controller/AdminController.php
+++ site/src/Controller/AdminController.php
@@ -1,14 +1,21 @@
 <?php
-
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\User;
+use App\Form\EditUserType;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
+/**
+ * @Route("/admin", name="admin_")
+ */
 class AdminController extends AbstractController
 {
     /**
-     * @Route("/admin", name="admin")
+     * @Route("/", name="adminHome")
      */
     public function index()
     {
@@ -16,4 +23,31 @@ class AdminController extends AbstractController
             'controller_name' => 'AdminController',
         ]);
     }
+    /**
+     * @Route("/users", name="interface_users")
+     */
+    public function usersList(UserRepository $user)
+    {
+        return $this->render('admin/user.html.twig', [
+            'users' => $user->findAll(),
+        ]);
+    }
+    
+    /**
+     * @Route("/utilisateurs/modifier/{id}", name="set_users")
+     */
+    public function editUser(Request $request, User $user, EntityManagerInterface $entity_manager)
+    {
+        
+        $form = $this->createForm(EditUserType::class, $user);
+        
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()) {
+            $entity_manager->flush();
+            
+            return $this->redirectToRoute('admin_interface_users');
+        }
+        
+        return $this->render('admin/editUser.html.twig', [ 'formUser' => $form->createView() ]);
+    } 
 }
---
+++ site/src/Form/EditUserType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+
+class EditUserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('email')
+            ->add('roles',ChoiceType::class, [ 
+                'choices' => [ 
+                    'Utilisateur' => 'ROLE_USER', 
+                    'Editeur' => 'ROLE_EDITOR', 
+           
+                 ],
+                 'expanded' => true,
+                 'multiple' => true,
+                 'label' => 'Roles' 
+            ]);                        
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array('data_class' => User::class));
+    }
+}
